07d54a7ed67d3a3861c4310f31ad0f975630ab43
JesusGarciaShakur <jesusrguez970@gmail.com>
2024-12-11 17:34:01 -0600

    add bd

--- App/models/db.py
+++ App/models/db.py
@@ -4,8 +4,8 @@ import mysql.connector
 def get_connection():
     mydb = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="",
+        user="shakur",
+        password="shaaragy970",
         database="sgipo1"
     )
     return mydb
